d9e5d0709ee6cc3f4ad47789f76ab499d616be33
Miguel Rubio <miguelantonio.rubio@gmail.com>
2016-05-25 22:59:21 +0000

    Rename css and js files. Update index.php file

--- index.php
+++ index.php
@@ -5,13 +5,13 @@
 	    <meta charset="utf-8">
 	    <meta name="viewport" content="width=device-width, initial-scale=1">
 	    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap.min.css">
-	    <link rel="stylesheet" href="css/rows.css">
+	    <link rel="stylesheet" href="css/rowcraft.css">
 	</head>
 	<body>
 		<div class="container">
 			<div class="window">
 				<div class="window-header">
-					<h5>Rows</h5>
+					<h5>Rowcraft</h5>
 				</div>
 				<div class="window-body">
 					<div class="output">
@@ -33,7 +33,7 @@
 	    <!-- Bootstrap JS -->
 	    <script src="//maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
 	    <!-- Rows main script -->
-	    <script src="js/rows.js"></script>
+	    <script src="js/rowcraft.js"></script>
 	</body>
 
 </html>
